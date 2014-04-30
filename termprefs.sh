634397b2a4aab2d23495dcc6b7812c1990cae5a9
Luis Armando Bianchin <luis.armandob@gmail.com>
2014-04-30 18:54:43 -0300

    Tmux config with safe-reattach-to-user-namespace, meat to work with linux

--- termprefs.sh
+++ termprefs.sh
@@ -11,6 +11,14 @@ alias gitsearch='git rev-list --all | xargs git grep -F'
 alias v='vim'
 
 # tmux
+function safe-reattach-to-user-namespace() {
+	# If reattach-to-user-namespace is not available, just run the command.
+	if [ -n "$(command -v reattach-to-user-namespace)" ]; then
+		reattach-to-user-namespace $@
+	else
+		exec "$@"
+	fi
+}
 alias tma='tmux attach -d -t'
 alias tmn='tmux new -s $(basename $(pwd))'
 alias tml='tmux list-sessions'
@@ -41,6 +49,7 @@ case `uname -s` in
 	alias swap_off="sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist"
 	alias swap_on="sudo launchctl load /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist"
 	alias swap_rm="sudo rm /private/var/vm/swapfile*"
+	export PATH="/usr/local/bin:$PATH"
 	# GNU Coreutils
  	[ -s "/usr/local/opt/coreutils/libexec/gnubin" ] && export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
  	[ -s "/usr/local/opt/coreutils/libexec/gnuman" ] && export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
@@ -49,6 +58,7 @@ case `uname -s` in
     alias ls='ls -F --color=auto'
     alias pbcopy='xclip -selection clipboard'
     alias pbpaste='xclip -selection clipboard -o'
+	~/Dropbox/dev/xcape/xcape -e 'Control_L=Escape'
     ;;
 esac
 
