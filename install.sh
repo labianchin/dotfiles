d6ed13f0618f2402df1829000a33d70698c6abf5
Luis Armando Bianchin <luis.armandob@gmail.com>
2014-09-11 22:32:34 -0300

    Install does not symlink to .dotfiles, bashrc does not duplicate entries

--- install.sh
+++ install.sh
@@ -3,50 +3,38 @@
 set -o errexit
 set -o nounset
 
-dir=$(cd $(dirname "$0"); pwd) # dotfiles directory
+readonly dir=$(cd $(dirname "$0"); pwd) # dotfiles directory
 
-BKP=~/dotfiles_old       # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="vimrc vim zshrc myterminalrc gitconfig tmux.conf tmux-osx.conf"
 xfiles="gvimrc xbindkeysrc conkyrc gtk-bookmarks"
 macfiles="slate"
 
-case `uname -s` in
-  Darwin)
-    files="$files $macfiles"
-    ;;
-  Linux)
-    # has xorg running?
-    [[ "$(ps --no-headers -C X)" ]] && files="$files $xfiles"
-    ;;
-esac
+readonly bkp_dir=~/dotfiles_old       # old dotfiles backup directory
+echo "Creating $bkp_dir folder for backup of any existing dotfiles in home"
+mkdir -p $bkp_dir
 
-# create dotfiles_old in homedir
-echo "Creating $BKP folder for backup of any existing dotfiles in ~"
-mkdir -p $BKP
-echo "...done"
-
-echo "Creating ~/.dotfiles folder"
-DOTFILES=~/.dotfiles
-mv $DOTFILES "$BKP"
-ln -s $dir $DOTFILES
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-echo "=== Installing all of these dotfiles ==="
-echo $files
-echo "=== ==="
-for file in $files; do
-    mv ~/.$file "$BKP"
-    ln -s $DOTFILES/$file ~/.$file
-done
-echo "Adding myterminalrc to bashrc"
-echo "source ~/.myterminalrc" | tee -a ~/.bashrc
+function symlink_files() {
+  local source=$1
+  local files=$2
+  local backup=$3/
+  # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+  echo "=== Symlinking all of these dotfiles ==="
+  echo $files
+  echo "=== ==="
+  for file in $files; do
+      mv ~/.$file "$backup"
+      ln -s $source/$file ~/.$file
+  done
+}
 
 case `uname -s` in
   Darwin)
+    symlink_files $dir "$files $macfiles" $bkp_dir
+
     echo "Running osx specific configuration for mac preferences"
     mkdir -p $BPK/mac_preferences/
-    prefdir=$DOTFILES/mac_preferences
+    prefdir=$dir/mac_preferences
     macpref=~/Library/Preferences
     for f in $prefdir/*; do
       file=$(basename $f);
@@ -56,14 +44,21 @@ case `uname -s` in
     git config --global credential.helper osxkeychain
     ;;
   Linux)
+    # has xorg running?
+    [[ "$(ps --no-headers -C X)" ]] && files="$files $xfiles"
+    symlink_files $dir "$files" $bkp_dir
+
     git config --global credential.helper cache
     ;;
 esac
 
 echo ""
-bash $DOTFILES/vim/install_vim.sh
+bash $dir/vim/install_vim.sh
 
 echo ""
+echo "Adding myterminalrc to bashrc"
+grep -Fq 'source ~/.myterminalrc' ~/.bashrc || echo 'source ~/.myterminalrc' >> ~/.bashrc
+
 function oh-my-zsh-install() {
   local ZSH="$HOME/.oh-my-zsh"
   echo "== ZSH configuration"
