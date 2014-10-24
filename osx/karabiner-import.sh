721cf242069b1b3deb9bc28270f964700b1dd222
Luis Armando Bianchin <luis.armandob@gmail.com>
2014-10-24 16:44:22 -0200

    Adds karabiner import for control as esc

---
+++ osx/karabiner-import.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.controlL2controlL_escape 1
+/bin/echo -n .
+$cli set bilalh.remap.f19_escape_control 1
+/bin/echo -n .
+/bin/echo
