fcb8bfcd3a10b5609457a151ff4dfe9c9f30d673
Luis Armando Bianchin <luis.armandob@gmail.com>
2015-02-12 13:53:14 -0200

    Updates brewfile and install_app, now with mirror

--- osx/install_apps.sh
+++ osx/install_apps.sh
@@ -4,6 +4,9 @@
 set -o nounset
 set -o errexit
 
+#export HOMEBREW_SOURCEFORGE_MIRROR='ufpr'
+export HOMEBREW_SOURCEFORGE_MIRROR='tcpdiag'
+
 if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -11,6 +14,7 @@ fi
 
 while read -r line; do
   [[ -z "$line" ]] && continue;
+  echo "~> Running brew $line"
   brew $line || true
 done <<< "$(grep -v '^$\|^\s*\#' Brewfile)"
 
