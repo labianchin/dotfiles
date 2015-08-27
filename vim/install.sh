e98a42f646bf924e80c0eb34e48791428f77d344
Luis Bianchin <labianchin@users.noreply.github.com>
2015-08-26 22:00:26 -0300

    vim: enhance vim install
    Output dependencies, improve symlink

--- vim/install.sh
+++ vim/install.sh
@@ -3,17 +3,29 @@
 set -o errexit
 set -o nounset
 
-parentdir=$(cd $(dirname "$0")/..; pwd)
+readonly CURRENT_DIR=$(cd $(dirname "$0"); pwd)
 
 echo "== VIM configuration"
-echo "=== The vim you have is the following: "
+echo "=== Vim current version is the following (make sure is >=7.4): "
 vim --version | head -1
 
-echo "=== Installing vim configuration (vimrc and vim folder from $parentdir)"
-mv ~/.vimrc ~/.vim /tmp/ || true
+echo "Make sure you have installed the following dependencies:
+Powerline fonts: https://github.com/powerline/fonts
+Ag silver searcher: https://github.com/ggreer/the_silver_searcher"
 
-ln -s $parentdir/vim ~/.vim
-ln -s ~/.vim/vimrc.vim ~/.vimrc
+readonly TARGET_VIM_DIR="$HOME/.vim"
+readonly TARGET_VIMRC="$HOME/.vimrc"
+
+echo "=== Installing vim configuration (vim folder from $CURRENT_DIR)"
+mv "$TARGET_VIMRC" "$TARGET_VIM_DIR" /tmp/ || true
+
+ln -s "$CURRENT_DIR" "$TARGET_VIM_DIR"
+ln -s "$TARGET_VIM_DIR/vimrc.vim" "$TARGET_VIMRC"
 
 echo "=== Installing vim plugins (using neobundle)"
 vim +NeoBundleInstall +qall
+
+
+# TODO: https://github.com/mutewinter/dot_vim
+# https://github.com/nathanaelkane/vim-indent-guides
+# add NeoVim install
