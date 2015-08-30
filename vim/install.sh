10673811fb9cdadac94b015c6244c7c73f0c2121
Luis Bianchin <labianchin@users.noreply.github.com>
2015-08-30 14:33:03 -0300

    vim: install also for nvim

--- vim/install.sh
+++ vim/install.sh
@@ -13,14 +13,17 @@ echo "Make sure you have installed the following dependencies:
 Powerline fonts: https://github.com/powerline/fonts
 Ag silver searcher: https://github.com/ggreer/the_silver_searcher"
 
-readonly TARGET_VIM_DIR="$HOME/.vim"
-readonly TARGET_VIMRC="$HOME/.vimrc"
+readonly TARGET_VIM="$HOME/.vim"
+readonly TARGET_NVIM="$HOME/.nvim"
 
-echo "=== Installing vim configuration (vimrc and vim folder from $CURRENT_DIR)"
-mv "$TARGET_VIMRC" "$TARGET_VIM_DIR" /tmp/ || true
+echo "=== Installing vim/nvim configuration (vimrc and vim folder from $CURRENT_DIR)"
+mv "${TARGET_VIM}rc" "$TARGET_VIM" "${TARGET_NVIM}rc" "$TARGET_NVIM" /tmp/ || true
 
-ln -s "$CURRENT_DIR" "$TARGET_VIM_DIR"
-ln -s "$TARGET_VIM_DIR/vimrc.vim" "$TARGET_VIMRC"
+ln -s "$CURRENT_DIR" "$TARGET_VIM"
+ln -s "$TARGET_VIM/vimrc.vim" "${TARGET_VIM}rc"
+
+ln -s "$CURRENT_DIR" "$TARGET_NVIM"
+ln -s "$TARGET_NVIM/vimrc.vim" "${TARGET_NVIM}rc"
 
 echo "=== Installing vim plugins (using neobundle)"
 vim +NeoBundleInstall +qall
