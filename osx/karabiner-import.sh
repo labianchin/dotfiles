0cf22387a7a604e5defb0931a437e15882bcba54
Luis Bianchin <labianchin@users.noreply.github.com>
2017-01-08 18:50:23 +0100

    karabiner-import

--- osx/karabiner-import.sh
+++ osx/karabiner-import.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# /Applications/Karabiner.app/Contents/Library/bin/karabiner export
+
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set remap.controlL2controlL_escape 1
-/bin/echo -n .
 $cli set bilalh.remap.f19_escape_control 1
+$cli set remap.iso_swap_tilda_section 1
+$cli set remap.fn_cursormove_to_pageup 1
+$cli set remap.option_r_to_right_click 1
+$cli set remap.shiftEscape2tilde 1
+$cli set repeat.initial_wait 250
+$cli set repeat.wait 33
+# ???
 /bin/echo -n .
 /bin/echo
