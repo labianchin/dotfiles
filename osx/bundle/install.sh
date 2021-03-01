2d0b47d4e028f8e14a78168408e594c8d22b0757
Luis Bianchin <labianchin@users.noreply.github.com>
2021-03-01 06:43:50 +0100

    Update brew installation

--- osx/bundle/install.sh
+++ osx/bundle/install.sh
@@ -13,7 +13,7 @@ xcode-select --print-path || xcode-select --install
 # https://apple.stackexchange.com/questions/254380/why-am-i-getting-an-invalid-active-developer-path-when-attempting-to-use-git-a
 
 if ! hash brew 2> /dev/null; then
-  echo "Installing homebrew..."
+  echo "Installing Homebrew https://brew.sh/ ..."
   /bin/bash -xc "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
@@ -26,39 +26,24 @@ taps() {
 
 brew_parallel() {
   echo "$@" | time xargs -n1 -P4 brew fetch
-  brew install "$@"
-}
-
-brew_cask_parallel() {
-  echo "$@" | time xargs -n1 -P4 brew cask fetch
-  brew cask install "$@" || true
-}
-
-setup_gnu_tools() {
-  brew_parallel curl readline fzf tmux rg coreutils gnu-sed make grep gnu-tar gnu-time gnu-which gnu-indent gnu-units findutils
-
-  mkdir -p /usr/local/etc/gnubin/ /usr/local/etc/gnuman/
-
-  for gnuutil in /usr/local/opt/**/libexec/gnubin/*; do
-      ln -sf "$gnuutil" /usr/local/etc/gnubin/
-  done
-
-  for gnuutil in /usr/local/opt/**/libexec/gnuman/*; do
-      ln -sf "$gnuutil" /usr/local/etc/gnuman/
-  done
+  brew install "$@" || true
 }
 
 main() {
   set -o xtrace
   export HOMEBREW_NO_ANALYTICS=1
-  taps
+  export HOMEBREW_NO_INSTALL_CLEANUP=1
+  time taps
 
   # fundamentals
-  time brew install neovim zsh coreutils findutils git readline || true
+  time brew install neovim zsh coreutils findutils git readline curl || true
   export HOMEBREW_NO_AUTO_UPDATE=1
-  brew_cask_parallel google-chrome keepassxc google-backup-and-sync kitty hammerspoon font-fira-code karabiner-elements corretto corretto8 slack dropbox osxfuse font-fira-code
-  brew_parallel curl readline fzf tmux rg gnu-sed docker python@3.8 maven emacs sqlite pandoc ruby libyaml borgbackup
-  setup_gnu_tools
+
+  time brew_parallel \
+      google-chrome keepassxc google-backup-and-sync kitty hammerspoon \
+      font-fira-code karabiner-elements corretto11 corretto8 dropbox osxfuse font-fira-code \
+      curl readline fzf tmux ripgrep coreutils gnu-sed make grep gnu-tar gnu-time gnu-which gnu-indent gnu-units findutils \
+      xz zlib sqlite3 make libyaml python@3 docker emacs borgbackup pandoc ruby openjdk maven \
 
   time brew bundle --verbose --file="$DIR/Brewfile"
   brew analytics off
